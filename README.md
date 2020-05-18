02dffff36183d6c893cb6ec9e42fff3d0bdef618
Uzay-G <uzgirit@gmail.com>
2020-05-18 22:15:02 +0200

    refactor code and docs

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 Simple wrapper for Prismanalytics Discord bot analytics that interfaces with `discord.py`. 
 
+## Setup
+Initialize the connection with our library like this:
+
+```python
+import prismapy
+
+client = discord.Client()
+analytics = prismapy.Prismalytics("<api key>", client)
+```
+
+And then, add this code to your `discordpy` bot's code:
+
+```python
+@client.event
+async def on_command(ctx):
+    analytics.send(ctx)
+```
+
+This will send the data to our api through the library and you'll have a beatiful dashboard like this:
+
+![demo](https://github.com/Uzay-G/prisma.py/blob/master/galena2.gif)
--- prismapy/__init__.py
+++ prismapy/__init__.py
@@ -4,6 +4,13 @@ import requests
 
 class Prismalytics:
     def __init__(self, key, client, save_server=True):
+        """
+        key: api token for prismalytics
+        save_server: option to save server data
+        client: discordpy client
+        commands: commands that will be recorded by the lib
+        servers: server data in which bot is used
+        """
         self.key = key
         self.save_server = save_server
         self.client = client
@@ -11,15 +18,10 @@ class Prismalytics:
         self.servers = []
         self.time = None
 
+    def process_data(self, message, server):
+        # processes data from discord into `servers` and `commands`
 
-    def send(self, ctx):
-        unset = False
-        if self.time is None:
-            self.time = datetime.datetime.now()
-            unset = True
-
-        message = ctx.message.content.split(" ")[0]
-        server = ctx.message.guild
+        message = message.content.split(" ")[0]
         if message in self.commands.keys():
             self.commands[message] += 1
         else:
@@ -29,16 +31,44 @@ class Prismalytics:
             match = next((i for i in self.servers if i["name"] == server.name), None)
             
             if match is None:
-                self.servers.append({"name": server.name, "member_count": server.member_count, "region": server.region, "bot_messages": 1})
+                new_server = {
+                    "name": server.name,
+                    "member_count": server.member_count,
+                    "region": server.region, "bot_messages": 1
+                    }
+                self.servers.append(new_server)
+
             else:
                 match["bot_messages"] += 1
+            
+    def send(self, ctx):
+        """
+        processes data and sends every 2 minutes
+        TODO: refactor so that users don't need to use ctx
+        """
+        unset = False
+        if self.time is None:
+            self.time = datetime.datetime.now()
+            unset = True
+
+        server = ctx.message.guild
+        self.process_data(ctx.message, server)
+        
 
         curr_time = datetime.datetime.now()
-        if (unset or ((curr_time - self.time).total_seconds() / 60) > 2):
+        interval = (curr_time - self.time).total_seconds() / 60
+        if (unset or interval > 2):
             self.time = curr_time
-            if self.save_server:
-                data = {'commands': json.dumps(self.commands), 'servers': json.dumps(self.servers), 'save_server': True}
-            else: data = {'commands': json.dumps(self.commands), 'save_server': False}
-            requests.post('https://prismalytics.herokuapp.com//send_data', data=data, headers={'key': self.key})
+            data = {
+                "commands": json.dumps(self.commands),
+                "save_server": self.save_server
+                }
+            if self.save_server: 
+                data["servers"] = json.dumps(self.servers)
+
+            # refactor this to use asyncio requests and have the program run in the background
+            requests.post('https://prismalytics.herokuapp.com/send_data', data=data, headers={'key': self.key})
+
+            # reinitialize stored data as it has been sent
             self.commands = {}
             self.servers = []
